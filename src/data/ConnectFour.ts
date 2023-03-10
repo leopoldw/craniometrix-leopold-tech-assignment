8728e127af3ed0e2a0d4f6a101e4e439b2b3245d
Leo W <leowicht@gmail.com>
2023-03-10 15:41:43 -0500

    algo done

--- src/data/ConnectFour.ts
+++ src/data/ConnectFour.ts
@@ -113,18 +113,17 @@ class ConnectFour {
   };
 
   calculateWinCondition = (column: ColumnCountType, row: RowCountType) => {
-    // was going to get fancy with recursion here to
-    // see if I could test all permutations across the board
-    // to find a win, but we can actually cut down the calculations
-    // needed every cycle considerably as we know exactly which space
-    // is now occupied with a player chip
-
     // firstly, we don't need to check for any win conditions before one user has
     // taken their 4th  (see formula above in const)
     // if (this.turnsTaken < NOT_ENOUGH_TURNS_FOR_WIN) {
     //   return false;
     // }
 
+    // was going to get fancy with recursion here to
+    // see if I could test all permutations across the board
+    // to find a win, but we can actually cut down the calculations
+    // needed every cycle considerably as we know exactly which space
+    // is now occupied with a player chip
     const winArrays = this.collectAllPossibleWinArrays(column, row);
 
     for (const array of winArrays) {
@@ -139,17 +138,6 @@ class ConnectFour {
       this.declareStalemate();
     }
   };
-
-  // iteration utility
-  //   times = <P, C extends number>(
-  //     count: number,
-  //     func: (index: number) => P
-  //   ): FixedLengthArray<P, C> => {
-  //     return [...Array(count)].map((_, index) => func(index)) as FixedLengthArray<
-  //       P,
-  //       C
-  //     >;
-  //   };
 
   collectAllPossibleWinArrays = (
     column: ColumnCountType,
@@ -172,7 +160,9 @@ class ConnectFour {
     }
 
     // calculate X-axis winner possibility
-    // there's only 4 it could (COLUMN_COUNT - WIN_LENGTH) be so add those to array
+    // there's only 4 it could be (COLUMN_COUNT - WIN_LENGTH)
+    // starting at column one as the first first position and
+    // column four as the last first position
     const XAxis = Array.from(
       { length: 4 },
       (_, rootIndex) =>
@@ -182,9 +172,47 @@ class ConnectFour {
     );
     possibleWinArrays.push(...XAxis);
 
+    // used for both diagonal tracking, determines
+    // where to start the forward or backward tracking
+    // by considering how high up the chip fell
+    const trackDistance = ROWS - row - 1;
+
     // calculate forward-facing diagonal winner possibility
-    // these do not happen if chip fell in top left of bottom right
-    // corner of the board (ie: )
+    // there's only 3 it could be (LONGEST DIAGONAL(6) - WIN_LENGTH + 1)
+    // first, trace the chip backwards diagonally to
+    // the bottom row and get the column
+    const forwardFacingStartingColumn = Math.max(1, column - trackDistance);
+
+    // then trace up & forwards 3 times
+    const forwardFacingAxis = Array.from(
+      { length: 3 },
+      (_, rootIndex) =>
+        Array.from({ length: 4 }, (_, rowIndex) =>
+          GV(
+            forwardFacingStartingColumn + rootIndex + rowIndex,
+            ROWS - rootIndex - rowIndex
+          )
+        ) as BoardSegmentType
+    );
+    possibleWinArrays.push(...forwardFacingAxis);
+
+    // calculate backwards-facing diagonal winner possibility
+    // just like above, can only be 3, but in the other direction
+    // same logic as above but we trace forwards
+    const backwardsFacingStartingColumn = Math.min(7, column + trackDistance);
+
+    const backwardsFacingAxis = Array.from(
+      { length: 3 },
+      (_, rootIndex) =>
+        Array.from({ length: 4 }, (_, rowIndex) =>
+          GV(
+            backwardsFacingStartingColumn - rootIndex - rowIndex,
+            ROWS - rootIndex - rowIndex - 1
+          )
+        ) as BoardSegmentType
+    );
+
+    possibleWinArrays.push(...backwardsFacingAxis);
 
     return possibleWinArrays;
   };
